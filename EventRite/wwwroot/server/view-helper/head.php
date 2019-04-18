b909f15bab26c6d11ad4b1c8826231091b05052d
Sharne <sharnegeorg@gmail.com>
2019-04-18 11:15:39 +0800

    Added google fonts link reference to the header view-helper

--- EventRite/wwwroot/server/view-helper/head.php
+++ EventRite/wwwroot/server/view-helper/head.php
@@ -8,6 +8,7 @@
         <title>EventRite</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="browser/css/styles.css" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans%7CRaleway:300,400" rel="stylesheet">
 
     </head>
     <body>';
