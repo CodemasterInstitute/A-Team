7782a94555d39c8a2dcf8859a009b9db83132198
Sharne <sharnegeorg@gmail.com>
2019-04-18 15:08:51 +0800

    amended logo in nav

--- EventRite/wwwroot/server/view-helper/navigation.php
+++ EventRite/wwwroot/server/view-helper/navigation.php
@@ -3,7 +3,7 @@
     print
     '
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <img class="navbar-brand mr-auto" src="images/redlogo.png">
+        <img class="navbar-brand mr-auto" <img alt="" src="/browser/images/redlogo.png" />
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
