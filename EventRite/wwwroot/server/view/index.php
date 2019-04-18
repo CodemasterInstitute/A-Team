debb82f669bb20f37409e79934a39383b3e45bb2
Mathew Cook <mat.cook@bigpond.com>
2019-04-18 11:30:36 +0800

    Added Search to Homepage

--- EventRite/wwwroot/server/view/index.php
+++ EventRite/wwwroot/server/view/index.php
@@ -20,6 +20,43 @@
     </div>
   </div>
 
+  <div class="search-sec">
+    <div class="container">
+        <form action="#" method="post" novalidate="novalidate">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <input type="text" class="form-control search-field" placeholder="Event Name">
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <input type="text" class="form-control search-field" placeholder="City">
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <select class="form-control search-field" id="exampleFormControlSelect1">
+                                <option>Category</option>
+                                <option>Music</option>
+                                <option>Theatre</option>
+                                <option>Family</option>
+                                <option>Comedy</option>
+                                <option>Sport</option>
+                                <option>Food & Wine</option>
+                                <option>Free</option>
+                                <option>Art</option>
+                                <option>Markets</option>
+                                <option>Other</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <button type="button" class="btn btn-danger wrn-btn">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
 <?php
     // Include Basic Templates for <footer>
     include "$_SERVER[DOCUMENT_ROOT]/server/view-helper/footer.php"; 
