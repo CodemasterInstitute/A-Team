7584d8bbdbcb7e2cac79898222e7bc1608a5d7a3
Sharne <sharnegeorg@gmail.com>
2019-04-18 15:01:05 +0800

    added logo to header

--- EventRite/wwwroot/server/view-helper/navigation.php
+++ EventRite/wwwroot/server/view-helper/navigation.php
@@ -3,7 +3,7 @@
     print
     '
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand mr-auto" href="#">EVENTRITE</a>
+        <img class="navbar-brand mr-auto" src="images/redlogo.png">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
