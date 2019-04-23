5ede7ffeace3ec944c1395cd33348ba360a72f3f
Mathew Cook <mat.cook@bigpond.com>
2019-04-23 14:59:36 +0800

    Added extra details to event cards

--- EventRite/wwwroot/server/view-helper/CardGenerator.php
+++ EventRite/wwwroot/server/view-helper/CardGenerator.php
@@ -10,8 +10,15 @@ class CardGenerator {
 
   function printCard() {
 
+    function truncate_phrase($phrase, $max_words) {
+      $phrase_array = explode(' ',$phrase);
+      if(count($phrase_array) > $max_words && $max_words > 0)
+         $phrase = implode(' ',array_slice($phrase_array, 0, $max_words)).'...';
+      return $phrase;
+   }
+
     foreach($this->$array as $val) {
-      // var_dump($val);
+      // var_dump($val);      
       ?>
         <div class="event-card col-lg-8">
           <div class="card-image">
@@ -20,9 +27,12 @@ class CardGenerator {
         <div class="card-content">
           <div class="card-title"><?php echo $val["event_name"] ?></div>
           <div class="card-description">
-          <?php
-            echo $val["description"] . ' <a href="/event.php?id=' . $val["id"] . '">Find out more</a>' ;
-          ?>
+            <?php echo truncate_phrase($val["description"], 30);?> <a href="/event.php?id=<?php echo $val["id"]?>">Find out more</a>
+          </div>
+          <div class="event-details">
+            <p><?php echo date('jS M Y', strtotime($val["start_date"]))?></p>
+            <p><?php echo $val["address"]?></p>
+            <p><?php echo $val["price"]?></p>
           </div>
           </div>
         </div>
