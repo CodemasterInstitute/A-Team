0a059ce1dba15745fa367698fe93f273e33809de
Mathew Cook <mat.cook@bigpond.com>
2019-04-23 13:57:25 +0800

    Add images to cards

--- EventRite/wwwroot/server/view-helper/CardGenerator.php
+++ EventRite/wwwroot/server/view-helper/CardGenerator.php
@@ -10,27 +10,18 @@ class CardGenerator {
 
   function printCard() {
 
-    function truncate($text, $chars = 300) {
-      if(strlen($text) > $chars) {
-          $text = $text.' ';
-          $text = substr($text, 0, $chars);
-          $text = substr($text, 0, strrpos($text ,' '));
-          $text = $text.'...';
-      }
-      return $text;
-  }
     foreach($this->$array as $val) {
       ?>
       
         <div class="event-card col-lg-8">
           <div class="card-image">
-          <img src="https://via.placeholder.com/200" alt="Event Card Image" />
+          <img src="<?php echo '/browser/images/events/' . $val['image'] ?>" alt="Event Card Image" />
         </div>
         <div class="card-content">
           <div class="card-title"><?php echo $val["event_name"] ?></div>
           <div class="card-description">
           <?php
-            echo truncate($val["description"]) . ' <a href="/event.php?id=' . $val["id"] . '">Find out more</a>' ;
+            echo $val["description"] . ' <a href="/event.php?id=' . $val["id"] . '">Find out more</a>' ;
           ?>
           </div>
           </div>
