244b6d462abbb21d23a7d88d14b41e1be2608d01
Sharne <sharnegeorg@gmail.com>
2019-04-23 14:38:17 +0800

    updated login link

--- EventRite/wwwroot/server/view-helper/navigation.php
+++ EventRite/wwwroot/server/view-helper/navigation.php
@@ -23,7 +23,7 @@
                   <a class="nav-link" href="contact.php">Contact</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="/members.index.php">Log In</a>
+                  <a class="nav-link" href="/login.php">Log In</a>
                 </li>
           </ul>
          
