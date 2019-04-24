7223a949f7ad22c50798ba6747d6329d48fb51ff
Benjamin Newton <bpnewton1@gmail.com>
2019-04-24 12:34:55 +0800

    Created basic members/index.php page

---
+++ EventRite/wwwroot/server/view/members/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    // Include Basic Templates for <head> and <header>
+    include "$_SERVER[DOCUMENT_ROOT]/server/view-helper/head.php"; 
+    include "$_SERVER[DOCUMENT_ROOT]/server/view-helper/navigation.php";
+?>
+    <div class="row">
+        <div class="col-0 col-md-1 col-lg-2 aside-left">';
+
+        
+    <!-- Navigation Bar Goes Here. -->
+
+
+    </div>
+        <div class="col-12 col-md-10 col-lg-8 middle">
+            <h1>Form Generated Content</h1>
+
+    <!-- Autopoluated information goes here. -->
+    
+        </div>
+        <div class="col-0 col-md-1 col-lg-2 aside-right">
+
+        </div>
+    </div>
+
+<?php
+    // Include Basic Templates for <footer>
+    include "$_SERVER[DOCUMENT_ROOT]/server/view-helper/footer.php"; 
+
+?>
