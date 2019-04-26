80c633647d6f7d4206792c5ee7f0a535d95288da
Sharne <sharnegeorg@gmail.com>
2019-04-26 16:15:10 +0800

    linked createaccount form to users table

--- EventRite/config/config.php
+++ EventRite/config/config.php
@@ -1,15 +1,15 @@
 <?php
 
-    // // Development Database Credentials
-    // $servername = "localhost";
-    // $username = "root";
-    // $password = "root";
-    // $database = "eventrite_local";
+    //Development Database Credentials
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "eventrite_local";
 
     // Online Development
-    $servername = "162.144.27.218";
-    $username = "benjaoa3_event";
-    $password = "codemaster";
-    $database = "benjaoa3_eventrite_development";
+        // $servername = "162.144.27.218";
+        // $username = "benjaoa3_event";
+        // $password = "codemaster";
+        // $database = "benjaoa3_eventrite_development";
 
 ?>
