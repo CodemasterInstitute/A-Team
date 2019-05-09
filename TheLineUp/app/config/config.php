1b0d4c0d17fb317d0f9abb1f08a1a66388213fc1
Benjamin Newton <bpnewton1@gmail.com>
2019-05-09 13:54:31 +0800

    URLROOT is now /thelineup and all relevant config files where changed

--- TheLineUp/app/config/config.php
+++ TheLineUp/app/config/config.php
@@ -8,6 +8,6 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost:8888/thelineup');
+  define('URLROOT', 'http://localhost:8888');
   // Site Name
   define('SITENAME', 'TheLineUp');
