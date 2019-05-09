f49cbfe15627d533bc4c56472275a4332d6e14b0
Sharne <sharnegeorg@gmail.com>
2019-05-09 11:12:40 +0800

    remove categories and advertise menu items.  update logo

--- EventRite/wwwroot/server/view-helper/navigation.php
+++ EventRite/wwwroot/server/view-helper/navigation.php
@@ -1,7 +1,7 @@
 <?php
 
-    print
-    '
+   print
+  '
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <img class="navbar-brand mr-auto" <img alt="" src="/browser/images/redlogo.png" />
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,12 +13,6 @@
             <li class="nav-item active">
               <a class="nav-link" href="index.php">Events <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="categories.php">Categories</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Advertise</a>
-              </li>
               <li class="nav-item">
                   <a class="nav-link" href="contact.php">Contact</a>
                 </li>
