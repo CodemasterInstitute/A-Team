bdefb147f271261499f41ca61401b25a89e51ae6
Benjamin Newton <bpnewton1@gmail.com>
2019-05-09 17:01:23 +0800

    Added update password section to details.

--- TheLineUp/app/views/dashboard/details.php
+++ TheLineUp/app/views/dashboard/details.php
@@ -17,8 +17,6 @@
                         <input class="dashboard-form-input" type="text" name="last_name" value="<?php echo $data['user']->last_name; ?>" />
                         <label>Email</label>
                         <input class="dashboard-form-input" type="text" name="email" value="<?php echo $data['user']->email; ?>" />
-                        <label>Password</label>
-                        <input class="dashboard-form-input" type="text" name="password" value="<?php echo $data['user']->password; ?>" />
                         <label>Phone Number</label>
                         <input class="dashboard-form-input" type="text" name="phone_number" value="<?php echo $data['user']->phone_number; ?>" />
                         <input type="hidden" name="type" value="edit" />
@@ -40,7 +38,20 @@
                         <input type="hidden" name="type" value="delete" />
                         <button class="dashboard-form-button" type="submit">Submit</button>
                     </form>
-
+                
+                <?php elseif (isset($_GET['type']) && $_GET['type'] == 'password'): ?>
+                    
+                    <h3>Update Your Password</h3>
+                    <form class="dashboard-form" method="post" action="details">
+                        <label>New Password: </label>
+                        <input class="dashboard-form-input" type="text" name="new-password" />
+                        <label>Confirm New Password: </label>
+                        <input class="dashboard-form-input" type="text" name="new-password-confirm" />
+                        <label>Current Password: </label>
+                        <input class="dashboard-form-input" type="text" name="password" />
+                        <input type="hidden" name="type" value="password" />
+                        <button class="dashboard-form-button" type="submit">Reset Password</button>
+                    </form>
 
                 <?php else: ?>
 
@@ -51,17 +62,14 @@
                     <p>First Name: <?php echo $data['user']->first_name; ?></p>
                     <p>Last Name: <?php echo $data['user']->last_name; ?></p>
                     <p>Email: <?php echo $data['user']->email; ?></p>
-                    <p>Password: <?php echo $data['user']->password; ?></p>
                     <p>Phone Number: <?php echo $data['user']->phone_number; ?></p>
 
                     <div class="row dashboard-link-container">
                         <div class="col-12">
-                        <a class="dashboard-link" href="details?type=edit">Edit</a>
-                        <a class="dashboard-link" href="details?type=delete">Delete</a>
+                        <a class="dashboard-link" href="details?type=edit">Update Details</a>
+                        <a class="dashboard-link" href="details?type=password">Change Password</a>
+                        <a class="dashboard-link" href="details?type=delete">Delete Account</a>
                     </div>
-                    <pre>
-                    <?php echo print_r($_SESSION['user']); ?>
-                    </pre>
                     </div>
                     
 
