58603b75e6cb1456fe53ddc4bdda4b641aa73e96
Sharne <sharnegeorg@gmail.com>
2019-05-09 15:03:49 +0800

    added categories to homepage

---
+++ TheLineUp/app/views/inc/categories.php
@@ -0,0 +1,41 @@
+<div class="container-fluid">
+          <h2> EVENT CATEGORIES</h2>
+      
+          <br>
+      <div class="container gallery">
+          
+          <div class="row">
+            <a href="/index.php?category=Art&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/art.png" class="img-fluid galleryimage">
+            </a>
+            <a href="/index.php?category=Music&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/music.png" class="img-fluid galleryimage">
+            </a>
+            <a href="/index.php?category=Market&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/market.png" class="img-fluid galleryimage">
+            </a>
+          </div>
+          <div class="row">
+            <a href="/index.php?category=Food+and+Drink&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/food.png" class="img-fluid galleryimage">
+            </a>
+            <a href="/index.php?category=Comedy&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/comedy.png" class="img-fluid galleryimage">
+            </a>
+            <a href="/index.php?category=Sport&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/sport.png" class="img-fluid galleryimage">
+            </a>
+          </div>
+          <div class="row">
+          <a href="/index.php?category=Family&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/family.png" class="img-fluid galleryimage">
+            </a>
+            <a href="/index.php?category=Theatre&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/theatre.png" class="img-fluid galleryimage">
+            </a>
+            <a href="/index.php?category=Other&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/other.png" class="img-fluid galleryimage">
+            </a>
+          </div>
+        </div>
+      </div>
--- TheLineUp/app/views/pages/index.php
+++ TheLineUp/app/views/pages/index.php
@@ -1,6 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <?php require APPROOT . '/views/inc/slider.php'; ?>
 <?php require APPROOT . '/views/inc/searchbar.php'; ?>
+<?php include APPROOT . '/views/inc/categories.php'; ?>
 <div class="container">
   <h1><?php echo $data['title']; ?></h1>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
