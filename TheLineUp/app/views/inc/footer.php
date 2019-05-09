c656340dac3d8d3a3b86645061af889ec15aa5b6
Be <bethmrivers@gmail.com>
2019-05-09 14:47:51 +0800

    Update Footer and styles

--- TheLineUp/app/views/inc/footer.php
+++ TheLineUp/app/views/inc/footer.php
@@ -1,41 +1,49 @@
-</div>
 <footer>
-  <div class="container">
+<div class="container">      
     <div class="row">
 
-      <div class="col-sm-4 footer-left">
-        <h4>About</h4>
-        <p>TheLineUp is an exciting platform to find, attend or list events in music, art, sport, theatre, food, drink
-          and more. Looking for something to do? Search for events by name, category or date. Planning an event? List it
-          with us for free!</p>
-      </div>
+        <div class="col-sm-4 footer-left">
+          <h4>About</h4>
+          <p>Eventrite is an exciting platform to find, attend or list events in music, art, sport, theatre, family, food and more.</p>
+          <p>Looking for something to do? Search for events by name, category or date.</p>
+          <p>Planning an event?  List it with us for free!</p>
+        </div>
 
-      <div class="col-sm-4 footer-middle">
-        <h4>Events</h4>
-        <ul class="list-unstyled text-light">
-          <li><a class="nav-link text-light" href="#">Upcoming Events</a></li>
-          <li><a class="nav-link text-light" href="#">Post an Event</a></li>
-          <li><a class="nav-link text-light" href="#">Advertise</a></li>
-          <li><a class="nav-link text-light" href="#">FAQs</a></li>
-          <li><a class="nav-link text-light" href="#">Terms & Conditions</a></li>
-          <li><a class="nav-link text-light" href="#">Read our Privacy Policy</a></li>
-        </ul>
+      
+        <div class="col-sm-4 footer-middle">
+          <h4>Events</h4>
+          <ul class="list-unstyled text-light">
+            <li>
+            <a class="nav-link text-light" href="#" >Categories</a>
+            </li>
+            <a class="nav-link text-light" href="#" >Advertise your event</a>
+            </li>
+            <li>
+            <a class="nav-link text-light" href="#" >My Account log in</a>
+            </li>
+            <li>
+            <a class="nav-link text-light" href="#" >FAQs</a>
+            </li>
+            <li>
+            <a class="nav-link text-light" href="#" >T&Cs</a>
+            </li>
+          </ul>
       </div>
 
       <div class="col-sm-4 footer-right">
         <h4>Connect With Us</h4>
         <ul class="list-unstyled text-light">
-          <li><a class="nav-link text-light" href="#"><i class="fab fa-facebook-f pr-1"></i> Facebook</a></li>
-          <li><a class="nav-link text-light" href="#">
-              <i class="fab fa-twitter pr-1"></i> Twitter</a></li>
-          <li><a class="nav-link text-light" href="#"><i class="fab fa-instagram pr-1"></i> Instagram</a></li>
-        </ul>
-      </div>
-      <div class="row copyright">
-        <p>© 2019 Codemaster A-Team.</p>
-      </div>
+        <li><a class="nav-link text-light" href="#" ><i class="fab fa-facebook-f pr-1"></i> Facebook</a></li>
+        <li><a class="nav-link text-light" href="#" >
+          <i class="fab fa-twitter pr-1"></i> Twitter</a></li>
+          <li><a class="nav-link text-light" href="#" ><i class="fab fa-instagram pr-1"></i> Instagram</a></li>
+</ul>
+        </div>
+        <div class="row copyright">
+        <p>© 2019 Codemaster Institute A-Team | The Line Up</p>  
+        </div>
     </div>
-  </div>
+</div>
 </footer>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"
--- TheLineUp/app/views/pages/index.php
+++ TheLineUp/app/views/pages/index.php
@@ -3,4 +3,5 @@
 <?php require APPROOT . '/views/inc/searchbar.php'; ?>
 <div class="container">
   <h1><?php echo $data['title']; ?></h1>
+</div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
