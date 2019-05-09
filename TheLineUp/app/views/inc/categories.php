d58b592cbe48ac2df42a906a059714315607e80b
Sharne <sharnegeorg@gmail.com>
2019-05-09 15:58:38 +0800

    update categories - delete market. include free

--- TheLineUp/app/views/inc/categories.php
+++ TheLineUp/app/views/inc/categories.php
@@ -11,8 +11,8 @@
             <a href="/index.php?category=Music&submit=submit" class="col-md-4 gallery">
               <img src="<?php echo URLROOT ?>/img/categories/music.png" class="img-fluid galleryimage">
             </a>
-            <a href="/index.php?category=Market&submit=submit" class="col-md-4 gallery">
-              <img src="<?php echo URLROOT ?>/img/categories/market.png" class="img-fluid galleryimage">
+            <a href="/index.php?category=Theatre&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/theatre.png" class="img-fluid galleryimage">
             </a>
           </div>
           <div class="row">
@@ -30,8 +30,9 @@
           <a href="/index.php?category=Family&submit=submit" class="col-md-4 gallery">
               <img src="<?php echo URLROOT ?>/img/categories/family.png" class="img-fluid galleryimage">
             </a>
-            <a href="/index.php?category=Theatre&submit=submit" class="col-md-4 gallery">
-              <img src="<?php echo URLROOT ?>/img/categories/theatre.png" class="img-fluid galleryimage">
+           
+            <a href="/index.php?category=Free&submit=submit" class="col-md-4 gallery">
+              <img src="<?php echo URLROOT ?>/img/categories/free.png" class="img-fluid galleryimage">
             </a>
             <a href="/index.php?category=Other&submit=submit" class="col-md-4 gallery">
               <img src="<?php echo URLROOT ?>/img/categories/other.png" class="img-fluid galleryimage">
