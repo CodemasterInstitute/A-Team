0f4a5d21ab9857e1089b1c7d447cfe5b3cc3fcdd
Benjamin Newton <bpnewton1@gmail.com>
2019-05-09 10:40:53 +0800

    Small changes

--- EventRite/wwwroot/server/view/members/details.php
+++ EventRite/wwwroot/server/view/members/details.php
@@ -26,20 +26,37 @@
                 </div>
                 <div class="col-9">
 
-                <!-- If type = edit display this -->
+                <?php $userID = $_SESSION['user_id']; ?>
+
+                <?php if (isset($_GET['type']) && $_GET['type'] = 'edit') : ?>
+
+                    <!-- If type = edit display this -->
+                    <!-- Add form with all palceholders filled -->
+
+                <?php elseif (isset($_GET['type']) && $_GET['type'] = 'delete') : ?>
+
+                    <!-- else if type = delete display this 
+                                create form that allows deleting user-->
+
+                <?php else : ?>
+
+                    <!-- else display normal type -->
 
                 
 
-                <!-- else if type = delete display this -->
-
-                <!-- else display normal type -->
-
-                    <h5>First Name</h5>
+                    <h5>First Name: </h5>
                     <h5>Last Name</h5>
                     <h5>Email</h5>
                     <h5>Address</h5>
                     <button>Update Details</button>
                     <button>Change Password</button>
+                    <form action="details.php" method="get" >
+                    <button type="submit" name="type" value="edit"></button>
+
+                    </form>
+
+
+                <?php endif; ?>
                 </div>
             </div>
         </div>
