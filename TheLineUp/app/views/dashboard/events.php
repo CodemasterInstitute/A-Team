489e27ec8fad1c7e4500e95911f73ef75f0d050a
Benjamin Newton <bpnewton1@gmail.com>
2019-05-09 20:45:08 +0800

    Basic create event form template

--- TheLineUp/app/views/dashboard/events.php
+++ TheLineUp/app/views/dashboard/events.php
@@ -2,9 +2,47 @@
 <?php include APPROOT . '/views/inc/memdash.php'; ?>
 
     <div class="col-9">
-    <?php if (isset($_GET['type']) && $_GET['type'] == 'edit'): ?>
+    <?php if (isset($_GET['type']) && $_GET['type'] == 'create'): ?>
 
-
+        <h3>Create Event</h3>
+        <form class="form-group">
+            <label>Event Name</label>
+            <input class="form-control" type="text" name="event_name" />
+            <label>Event Category</label>
+            <select class="form-control" name="category_id">
+                <option></option>
+                <option></option>
+            </select>
+            <input type="hidden" name="user_id" value="<?php echo $_SESSION['user']->user_id; ?>" />
+            <label>Start Date</label>
+            <input class="form-control" type="text" name="start_date_year" placeholder="Year" />
+            <input class="form-control" type="text" name="start_date_month" placeholder="Month" />
+            <input class="form-control" type="text" name="start_date_day" placeholder="Day" />
+            <label>Start Time</label>
+            <input class="form-control" type="text" name="start_time_hour" placeholder="Hour" />
+            <input class="form-control" type="text" name="start_date_minute" placeholder="Minute" />
+            <br/>
+            <label>End Date</label>
+            <input class="form-control" type="text" name="end_date_year" placeholder="Year" />
+            <input class="form-control" type="text" name="end_date_month" placeholder="Month" />
+            <input class="form-control" type="text" name="end_date_day" placeholder="Day" />
+            <label>End Time</label>
+            <input class="form-control" type="text" name="end_time_hour" placeholder="Hour" />
+            <input class="form-control" type="text" name="end_date_minute" placeholder="Minute" />
+            <br/>
+            <label>Price Per Ticket</label>
+            <input class="form-control" type="text" name="event_price" />
+            <label>Total Tickets</label>
+            <input class="form-control" type="text" name="tickets_available" />
+            <label>Event Description</label>
+            <textarea class="form-control" name="event_description">
+            </textarea>
+            <label>Event Image</label>
+            <input class="form-control" type="file" name="event_image" />
+            <label class="form-check-label">Featured</label>
+            <input class="form-check-input" type="checkbox" name="is_featured" />
+            <button class="dashbaord-form-button" type="submit">Create</button>
+        </form>
 
     <?php elseif (isset($_GET['type']) && $_GET['type'] == 'delete'): ?>
 
@@ -18,7 +56,7 @@
         <?php else: ?>
 
         <h3>Your Events</h3>
-        <a class="members-link">Create New Event</a>
+        <a href="events?type=create" class="members-link">Create New Event</a>
         <hr/>
             <div class="row">
                 <div class="col-9">
