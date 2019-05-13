67b4c07e668ca998ccfdeab4ff59f165bc8dd27c
Sharne <sharnegeorg@gmail.com>
2019-05-13 10:03:08 +0800

    initial commit  404 page

---
+++ TheLineUp/app/views/pages/notfound.php
@@ -0,0 +1,18 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<div class="jumbotron jumbotron-fluid post">
+<div class="container">
+
+<div class="row">
+    <h1 class ="notfound">OOPS!</h1>
+    <h2 class="notfound"> Something went wrong</h2>
+        <a href='/pages/index.php'>Take me back</a>
+</div>
+</div>
+
+  </div>
+
+
+
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
